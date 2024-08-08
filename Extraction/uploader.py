d08f1436cf3abb390210622f5e22e95a3de64cc1
medhruv27 <98713405+medhruv27@users.noreply.github.com>
2024-08-08 15:47:40 +0530

    Update uploader.py
    clear input output change

--- Extraction/uploader.py
+++ Extraction/uploader.py
@@ -4,6 +4,7 @@ from flask import Flask , request , jsonify
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'Input'
+OUTPUT_FOLDER = 'output'
 ALLOWED_EXTENSIONS = set(['png','jpg','jpeg'])
 
 def allowed_file(filename):
@@ -11,6 +12,7 @@ def allowed_file(filename):
 
 uploader = Flask(__name__)
 uploader.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+uploader.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 @uploader.route('/media/upload',methods = ['POST'])
 def upload_media():
@@ -32,12 +34,22 @@ def upload_media():
         try:
             result = subprocess.run(['python', 'extract_faces.py'], check=True, capture_output=True, text=True)
             output = result.stdout
+            if "Identity Verified!" in output:
+                output = "Identity Verified!"
+            else:
+                output = "Verification failed or not determined."
             return jsonify({'msg': 'media uploaded and processed successfully', 'output': output})
-            # return jsonify({'msg': 'media uploaded and processed successfully'})
         except subprocess.CalledProcessError:
             return jsonify({'error': 'error occurred during face extraction and verification'}), 500
-    
-    return jsonify({'error': 'error occurred during face extraction and verification'}), 500
+        finally:
+            for filename in os.listdir(uploader.config['UPLOAD_FOLDER']):
+                file_path = os.path.join(uploader.config['UPLOAD_FOLDER'], filename)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+            for filename in os.listdir(uploader.config['OUTPUT_FOLDER']):
+                file_path = os.path.join(uploader.config['OUTPUT_FOLDER'], filename)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
 
 if __name__ == '__main__':
     uploader.run(debug=True,port=5000, host='0.0.0.0')
