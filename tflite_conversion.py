6eb3abbac31097c0f20a64fbbc427b6d568e262b
Dhruv Verma <98713405+medhruv27@users.noreply.github.com>
2024-11-21 11:33:42 +0530

    Delete tflite_conversion.py

--- tflite_conversion.py
+++
@@ -1,9 +0,0 @@
-import os
-
-HOME = os.getcwd()
-
-from ultralytics import YOLO
-
-model = YOLO('best.pt') #path to pytorch model made using yolo
-
-model.export(format='tflite') #tflite model gets saved as best_float32.tflite
